e58dd6d463ec8755f712421ef7101c6da0f433f0
Jason_Shu <shuyue524@gmail.com>
2022-04-17 22:53:05 +0800

    完成express 官方例子

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.send("Hello 123123!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
